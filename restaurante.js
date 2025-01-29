5e4ecbb9aa3dd7de4b663f005b3fe4c8925d8f47
Guilomilo1 <guilherme2.laurindo.1bmangal@gmail.com>
2025-01-29 16:56:42 +0000

    .

---
+++ restaurante.js
@@ -0,0 +1,13 @@
+//cria referencia do botao calcular
+let btCalcular = document.getElementById('btCalcular');
+//registra o evento click associado ao botao
+btCalcular.addEventListener('click', calcularPreco);
+function calcularPreco(){
+    let inQuilo = document.getElementById('inQuilo');
+    let inConsumo = document.getElementById('inConsumo');
+    let outValor = document.getElementById('outValor');
+    let quilo = Number(inQuilo.value);
+    let consumo = Number(inConsumo.value);
+    let valor = (quilo/1000)*consumo;
+    outValor.innerHTML = 'Valor a ser pago: R$'+valor.toFixed(2);
+}
